d2627d867aede3f91c167351d4ea7d184ac2173d
Valentina Risso Vega <VALENTINA.RISSO@correo.ucu.edu.uy>
2026-04-13 20:34:12 -0300

    cambios estándares codificación

--- mi-proyecto/demo/src/main/java/uy/edu/curso/Ej26/Expresion.java
+++ mi-proyecto/demo/src/main/java/uy/edu/curso/Ej26/Expresion.java
@@ -5,24 +5,22 @@ import java.util.Stack;
 
 public class Expresion {
 private Stack<Character> pila; //utilización de la clase Stack de Java
-    public Expresion(){
+    public Expresion() {
         pila=new Stack<Character>();
     }
-    public boolean controlCorchetes(List<Character> listaDeEntrada)
-    {
-        for(int i=0; i<listaDeEntrada.size();i++)//recorre cada character de la lista.
-        {
-            if(listaDeEntrada.get(i)=='{'){ //agrega a la pila el character correspondiente
+    public boolean controlCorchetes(List<Character> listaDeEntrada) {
+        for(int i=0; i<listaDeEntrada.size();i++) {//recorre cada character de la lista.
+            if(listaDeEntrada.get(i)=='{') { //agrega a la pila el character correspondiente
                 pila.push('{');
             }
-            else if (listaDeEntrada.get(i)=='}'){
+            else if (listaDeEntrada.get(i)=='}') {
                 pila.push('}');
             }
-            if((pila.size()>1)&&(pila.peek()=='}')){ //como los corchetes agregados a la pila quedan en orden inverso (el último elemento de la lista, es el tope de la pila), se fija si el +ultimo agregado es un corchete de cierre
+            if((pila.size()>1)&&(pila.peek()=='}')) { //como los corchetes agregados a la pila quedan en orden inverso (el último elemento de la lista, es el tope de la pila), se fija si el +ultimo agregado es un corchete de cierre
                 pila.pop(); //si lo es, lo elimina
-                if(pila.peek()=='{'){ //si el ahora último (antes penúltimo) de la pila es un corchete de apertura, también lo elimina, de esta manera, si la secuencia es correcta, al final la pila queda vacía
+                if(pila.peek()=='{') { //si el ahora último (antes penúltimo) de la pila es un corchete de apertura, también lo elimina, de esta manera, si la secuencia es correcta, al final la pila queda vacía
                     pila.pop();
-                } else{
+                } else {
                     pila.push('}'); //sino vuelve a agregar el corchete de cierre y la lista vuelve a estar igual que antes de entrar al if
                 }
             }
