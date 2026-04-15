8daa77ea0fa5abb2ae8a10886c3ef010925bc4a6
risso <valentina.risso@correo.ucu.edu.uy>
2026-04-15 20:40:36 -0300

    Algunos cambios en carpetas, main de Expresion

---
+++ mi-proyecto/UT01/src/main/java/uy/edu/curso/Ej26/App.java
@@ -0,0 +1,19 @@
+package uy.edu.curso.Ej26;
+
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        Expresion exp = new Expresion();
+        List<Character> lista;
+        lista = List.of('{', '{', '}', '}');
+        System.out.println(exp.controlCorchetes(lista)); //debería imprimir "true"
+        lista = List.of('{', '{', '}', '}', '}', '}');
+        System.out.println(exp.controlCorchetes(lista)); //debería imprimir "false"
+        lista = List.of('{', '{', '}', '}', '{', '}', '{', '{', '}', '}');
+        System.out.println(exp.controlCorchetes(lista)); //debería imprimir "true"
+        lista = List.of('}');
+        System.out.println(exp.controlCorchetes(lista)); //debería imprimir "false"
+    }
+}
---
+++ mi-proyecto/UT01/src/main/java/uy/edu/curso/Ej26/Expresion.java
@@ -0,0 +1,31 @@
+package uy.edu.curso.Ej26;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Expresion {
+
+    private Stack<Character> pila=new Stack<>(); //utilización de la clase Stack de Java
+    public Expresion() {
+    }
+
+    public boolean controlCorchetes(List<Character> listaDeEntrada) {
+        pila.clear(); //se asegura que la pila esté vacía para evitar errores
+        for (int i = 0; i < listaDeEntrada.size(); i++) {//recorre cada character de la lista.
+            if (listaDeEntrada.get(i) == '{') { //agrega a la pila el character correspondiente
+                pila.push('{');
+            } else if (listaDeEntrada.get(i) == '}') {
+                pila.push('}');
+            }
+            if ((pila.size() > 1) && (pila.peek() == '}')) { //como los corchetes agregados a la pila quedan en orden inverso (el último elemento de la lista, es el tope de la pila), se fija si el +ultimo agregado es un corchete de cierre
+                pila.pop(); //si lo es, lo elimina
+                if (pila.peek() == '{') { //si el ahora último (antes penúltimo) de la pila es un corchete de apertura, también lo elimina, de esta manera, si la secuencia es correcta, al final la pila queda vacía
+                    pila.pop();
+                } else {
+                    pila.push('}'); //sino vuelve a agregar el corchete de cierre y la lista vuelve a estar igual que antes de entrar al if
+                }
+            }
+        }
+        return (pila.isEmpty());
+    }
+}
